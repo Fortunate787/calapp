73263b4a9aa2eabd736c794ffde553abb5b433aa
fortun8te <155467770+fortun8te@users.noreply.github.com>
2025-06-05 00:10:04 +0200

    Initial Commit

---
+++ CALAPP/CALAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CALAPPApp.swift
+//  CALAPP
+//
+//  Created by Michael Knaap on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CALAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
